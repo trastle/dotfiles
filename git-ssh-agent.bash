bcf97103d89cb51de4d714caa009d6f8c4920812
Troy Astle <troy.astle@gmail.com>
2013-04-07 16:36:20 +0100

    namespace functions as they are globally in my shell

--- git-ssh-agent.bash
+++ git-ssh-agent.bash
@@ -5,14 +5,27 @@
 SSH_ENV="$HOME/.ssh/environment"
   
 # start the ssh-agent
-function start_agent {
-    echo "Initializing new SSH agent..."
+function git-ssh-start-agent {
+    echo "No SSH agent running, initializing new SSH agent..."
     # spawn ssh-agent
     ssh-agent | sed 's/^echo/#echo/' > "$SSH_ENV"
-    echo succeeded
+    echo "New SSH agent successfully initialised, adding keys..."
     chmod 600 "$SSH_ENV"
     . "$SSH_ENV" > /dev/null
     git-ssh-add-keys
+}
+  
+# test for identities
+function git-ssh-test-identities {
+    # test whether standard identities have been added to the agent already
+    ssh-add -l | grep "The agent has no identities" > /dev/null
+    if [ $? -eq 0 ]; then
+        git-ssh-add-keys
+        # $SSH_AUTH_SOCK broken so we start a new proper agent
+        if [ $? -eq 2 ];then
+            git-ssh-start-agent
+        fi
+    fi
 }
 
 # Import all of my ssh keys into the agent
@@ -20,20 +33,7 @@ function git-ssh-add-keys {
     find ~/.ssh/. | grep _rsa$ | xargs ssh-add $1
     find ~/.ssh/. | grep _dsa$ | xargs ssh-add $1
 }
-  
-# test for identities
-function test_identities {
-    # test whether standard identities have been added to the agent already
-    ssh-add -l | grep "The agent has no identities" > /dev/null
-    if [ $? -eq 0 ]; then
-        git-ssh-add-keys
-        # $SSH_AUTH_SOCK broken so we start a new proper agent
-        if [ $? -eq 2 ];then
-            start_agent
-        fi
-    fi
-}
-  
+ 
 # check for running ssh-agent with proper $SSH_AGENT_PID
 if [ -n "$SSH_AGENT_PID" ]; then
     ps -ef | grep "$SSH_AGENT_PID" | grep ssh-agent > /dev/null
@@ -48,10 +48,8 @@ else
     fi
     ps -ef | grep "$SSH_AGENT_PID" | grep -v grep | grep ssh-agent > /dev/null
     if [ $? -eq 0 ]; then
-        test_identities
+        git-ssh-test-identities
     else
-        start_agent
+        git-ssh-start-agent
     fi
 fi
-
-
